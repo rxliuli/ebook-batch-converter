5c39f56afa7c49f73f827dc52f9a67915ce5c06c
rxliuli <rxliuli@gmail.com>
2021-07-14 17:13:47 +0800

    feat(ebook-batch-converter): 对已经转换过的文件不再转换

--- apps/cli/src/batchConvert.ts
+++ apps/cli/src/batchConvert.ts
@@ -1,6 +1,6 @@
 import promise from 'glob-promise'
 import * as path from 'path'
-import { mkdirp } from 'fs-extra'
+import { mkdirp, pathExists } from 'fs-extra'
 import { AsyncArray, asyncLimiting } from '@liuli-util/async'
 import { convert } from 'node-ebook-converter'
 import { fileName } from './util/fileName'
@@ -14,15 +14,20 @@ export async function batchConvert(input: string, output: string) {
   await AsyncArray.forEach(
     list,
     asyncLimiting(async (relativePath) => {
-      const destFilePath = path.join(output, relativePath)
+      const destFilePath = path.resolve(
+        output,
+        path.dirname(relativePath),
+        fileName(relativePath) + '.azw3',
+      )
+      if (await pathExists(destFilePath)) {
+        console.log('文件已转换: ', relativePath)
+        return
+      }
       await mkdirp(path.dirname(destFilePath))
       try {
         await convert({
           input: path.resolve(input, relativePath),
-          output: path.resolve(
-            path.dirname(destFilePath),
-            fileName(destFilePath) + '.azw3',
-          ),
+          output: destFilePath,
         })
       } catch (e) {
         console.error('转换出错: ', relativePath)
