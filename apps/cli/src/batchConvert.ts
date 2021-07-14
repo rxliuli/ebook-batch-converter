738d1205e59d42618702c50df0511741ae6793d1
rxliuli <rxliuli@gmail.com>
2021-07-14 18:04:39 +0800

    docs(ebook-batch-converter): 添加基本的国际化支持

--- apps/cli/src/batchConvert.ts
+++ apps/cli/src/batchConvert.ts
@@ -5,6 +5,7 @@ import { AsyncArray, asyncLimiting } from '@liuli-util/async'
 import { fileName } from './util/fileName'
 import { convert } from './util/convert'
 import chalk from 'chalk'
+import { i18n } from './util/I18n'
 export async function batchConvert(input: string, output: string) {
   const list = await promise('**/*.{epub,pdf,txt}', {
     cwd: path.resolve(input),
@@ -20,7 +21,7 @@ export async function batchConvert(input: string, output: string) {
         fileName(relativePath) + '.azw3',
       )
       if (await pathExists(destFilePath)) {
-        console.log(chalk.blue('文件已转换: ', relativePath))
+        console.log(chalk.blue(i18n.t('msg.skip'), relativePath))
         return
       }
       await mkdirp(path.dirname(destFilePath))
@@ -29,9 +30,9 @@ export async function batchConvert(input: string, output: string) {
           input: path.resolve(input, relativePath),
           output: destFilePath,
         })
-        console.log(chalk.green('转换成功: ', relativePath))
+        console.log(chalk.green(i18n.t('msg.success'), relativePath))
       } catch (e) {
-        console.error(chalk.red('转换出错: ', relativePath))
+        console.error(chalk.red(i18n.t('msg.error'), relativePath))
       }
     }, 1),
   )
--- apps/cli/src/bin.ts
+++ apps/cli/src/bin.ts
@@ -1,11 +1,28 @@
 import { Command } from 'commander'
 import { batchConvert } from './batchConvert'
+import { i18n, LanguageEnum } from './util/I18n'
+import osLocale from 'os-locale'
 
-new Command()
-  .description('使用 ebook-converter 批量转换 epub 等文件为 azw3')
-  .requiredOption('-i, --input <input>', '输入目录')
-  .requiredOption('-o, --output <output>', '输出目录')
-  .action(async (options: { input: string; output: string }) => {
-    await batchConvert(options.input, options.output)
-  })
-  .parse()
+async function getLanguage() {
+  const language = await osLocale()
+  const map: Record<string, LanguageEnum> = {
+    'zh-CN': LanguageEnum.ZhCN,
+    'en-US': LanguageEnum.EnUS,
+  }
+  return map[language] || LanguageEnum.EnUS
+}
+
+async function main() {
+  await i18n.load(await getLanguage())
+  new Command()
+    .description(i18n.t('cli.description'))
+    .requiredOption('-i, --input <input>', i18n.t('cli.input'))
+    .requiredOption('-o, --output <output>', i18n.t('cli.output'))
+    .action(async (options: { input: string; output: string }) => {
+      await batchConvert(options.input, options.output)
+    })
+    .parse()
+}
+
+// noinspection JSIgnoredPromiseFromCall
+main()
