0aab783e2fb60749b64db20bf6e62136a67affd8
rxliuli <rxliuli@gmail.com>
2021-07-15 18:43:48 +0800

    fix: 修复 i18n 文件没有捆绑的错误

--- i18n/i18nUtil.go
+++ i18n/i18nUtil.go
@@ -1,6 +1,7 @@
 package i18nUtil
 
 import (
+	"embed"
 	"encoding/json"
 	"github.com/Xuanwo/go-locale"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
@@ -10,6 +11,17 @@ import (
 
 var bundle *i18n.Bundle
 
+//go:embed en.json zh.json
+var i18nES embed.FS
+
+func addLocale(path string) {
+	bytes, err := i18nES.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	bundle.MustParseMessageFileBytes(bytes, path)
+}
+
 func init() {
 	tag, _ := locale.Detect()
 	if strings.Index(strings.ToLower(tag.String()), "zh") != -1 {
@@ -18,8 +30,8 @@ func init() {
 		bundle = i18n.NewBundle(language.English)
 	}
 	bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
-	bundle.MustLoadMessageFile("i18n/en.json")
-	bundle.MustLoadMessageFile("i18n/zh.json")
+	addLocale("en.json")
+	addLocale("zh.json")
 }
 
 func T(key string) string {
